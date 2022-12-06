37e9522a0052ad76cff24980e4035a62636b5dcf
Daniel Jackson <daniiel.cavallcante@gmail.com>
2022-12-06 20:48:45 -0300

    Resolve conflics

--- database.py
+++
@@ -1,2 +0,0 @@
-list_of_words = ["ética", "plena", "mútua", "tênue", "sutil", "vigor", "fazer", "aquém", "assim", "porém", "seção",
-                 "audaz", "sanar", "cerne", "fosse", "inato", "ideia", "poder", "moral", "desde", "justo", "muito", "torpe", "honra"]
