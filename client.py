ffca6186d13ccd8a8a184cfb54d2c5ad98986a44
Daniel Jackson <daniiel.cavallcante@gmail.com>
2022-12-11 14:00:48 -0300

    Add back the clearConsole function

--- client.py
+++ client.py
@@ -24,6 +24,9 @@ def clearConsole():
     os.system(command)
 
 while True:
+    # Limpar console
+    clearConsole()
+    
     # Mensagem recebida do servidor
     received_message = socket.recv(BUFF_SIZE)
     
@@ -112,6 +115,7 @@ while True:
             
             socket.send(message)
             
+            
             break
         else:
             print(received_message['content']['message'])
@@ -132,52 +136,4 @@ while True:
         print(received_message['content'])
     else:
         print('Erro desconhecido')  
-    
 
-    # word = input('Diga a palavra aí, vá:\n')
-    
-    # word = word.strip().replace(' ', '').upper()
-    
-    # if not word:
-    #     continue
-
-    # # Enviar dados para o servidor, portanto, enviando a palavra
-    # socket.send(word.encode())
-
-    # # Receber os dados que vem do servidor ate tamanho X
-    # # resposta sobre a palavra que a gt enviou anteriormente
-    # # 1 = caracter inicial na resposta
-    # # 6 = tamanho maximo da palvra de retorno 5 + 1 que é o \n
-
-    # # portanto o tabuleiro todo preenchido = 6 * 6 + 1 = 37
-    # # resposta sobre como o tabuleiro ficou e precisa decodar pois ele veio encodado
-    # response = socket.recv(BUFF_SIZE).decode()
-
-    # print('Menssagem ecoada:')
-
-    # words_board = response[1:]
-    # option = response[0]
-
-    # # limpar console
-    # clearConsole()
-
-    # # printa o tabuleiro atual
-    # print('-------------------\n')
-    # print(words_board)
-    # print('-------------------\n')
-
-    # if (option == "1"):
-    #     print('Ganhou')
-    #     socket.close()
-    #     break
-    # elif (option == "2"):
-    #     print('Perdeu')
-    #     socket.close()
-    #     break
-    # elif (option == "3"):
-    #     print('Palavra Errada')
-    # elif (option == "4"):
-    #     print('Palavra Invalida')
-    # else:
-    #     print('Número de tentativas esgotado')
-    #     socket.close()
