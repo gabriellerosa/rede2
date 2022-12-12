e7e919c0db992b2fed69d4cfd0889a6b52dfa698
Daniel Jackson <daniiel.cavallcante@gmail.com>
2022-12-12 18:11:00 -0300

    Better code formatting on client.py

--- client.py
+++ client.py
@@ -3,20 +3,70 @@ import socket
 import os
 import pickle
 import inquirer
+import time
 
 from rich.console import Console
 from rich.text import Text
-from rich.emoji import Emoji
 import rich.spinner as spinner
 from rich import box
 from rich.table import Table
-
-from config import *
 from termcolor import colored
 
-import time
+from config import *
 
 console = Console()
+
+wordle = '[green]W[yellow]O[white]R[green]D[yellow]L[white]E'
+    
+def show_table(board, secret_word):
+    
+    table = Table(
+                  show_header=False, 
+                  title=wordle, 
+                  box=box.SQUARE, 
+                  show_lines=True, 
+                )
+    
+    for tried_word in board:
+        validated_word = list()
+        
+        for i in range(len(tried_word)):
+            color = 'white'
+            
+            if(tried_word[i] == secret_word[i]):
+                color = 'green'
+            elif tried_word[i] in secret_word:
+                color = 'yellow'
+                
+            validated_word.append({
+                'letter': tried_word[i],
+                'color': color
+            })
+            
+        table.add_row(
+            *[
+                Text(
+                    letter['letter'], 
+                    style='bold ' + letter['color']
+                    ) 
+                for letter in validated_word
+            ], 
+        )
+        
+    for i in range(len(board), 6):
+        table.add_row(*[f" " for letter in secret_word])
+        
+    console.print(table)
+
+def clearConsole():
+    
+    command = 'clear'
+    
+    # Verifica se o sistema operacional é Windows
+    if os.name in ('nt', 'dos'):  
+        command = 'cls'
+
+    os.system(command)
 
 # Cria um socket TCP/IP
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,58 +82,14 @@ try:
             
     
     socket.connect((HOST, PORT))
-    console.log('[bold green]Conexão com o servidor estabelecida! :white_check_mark:')
+    console.log('[bold green]Conexão estabelecida com o servidor! :white_check_mark:')
     time.sleep(1)
     
 except ConnectionRefusedError:
     console.log('O servidor não está rodando!', style='bold red')
     exit()
-    
-    
-def show_table(board, secret_word):
-    table = Table(show_header=False, title="WORDLE", box=box.SQUARE, show_lines=True, header_style="bold magenta")
-    
-    for tried_word in board:
-        validated_word = list()
-        
-        for i in range(len(tried_word)):
-            if(tried_word[i] == secret_word[i]):
-                validated_word.append({
-                    'letter': tried_word[i], 
-                    'color': 'green'})
-            elif tried_word[i] in secret_word:
-                validated_word.append({
-                    'letter': tried_word[i], 
-                    'color': 'yellow'})
-            else:
-                validated_word.append({
-                    'letter': tried_word[i], 
-                    'color': 'white'})
-                
-            
-        table.add_row(
-            *[Text(letter['letter'], style='bold ' + letter['color']) for letter in validated_word], 
-        )
-        
-    for i in range(len(board), 6):
-        table.add_row(*[f" " for letter in secret_word])
-        
-    console.print(table)
-
-
-def clearConsole():
-    
-    command = 'clear'
-    # verificando qual o tipo de sistema operacional
-    
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-        command = 'cls'
-
-    # isso é como se a gt estivesse escrevendo no terminal, mas a maquina que escreve que maneiro =D
-    os.system(command)
 
 while True:
-    # Limpa o console
     clearConsole()
     
     # Mensagem recebida do servidor
@@ -93,67 +99,52 @@ while True:
     received_message = pickle.loads(received_message)
     msg_type = received_message['type']
     
+    # Dicionário que será enviado ao servidor
+    message = dict()
+    
     # Escolha de nickname
     if(msg_type == 'nickname_selection'):
-        wordle = 'Wordle'
-        colored_wordle = ""
-        colors = ['green', 'yellow', 'white']
-        
-        for i in range(len(wordle)):
-            colored_wordle += colored(wordle[i], colors[i % len(colors)])
-            
-        print('Bem vindo ao ' + colored_wordle + '!\n')
+        console.print('Bem vindo ao ' + wordle + '!\n')
         print('Escolha um nickname para começar a jogar!\n')
         
         nickname = input('Nickname: ')
         
-        # Send the nickname to the server
-        message = pickle.dumps({
-            'type': 'nickname_selection',
-            'content': nickname
-        })
-        
-        socket.send(message)
+        message['type'] = 'nickname_selection'
+        message['content'] = nickname
        
-    # Seleção de dificuldade 
+    # Escolha de dificuldade 
     elif(msg_type == 'difficulty_selection'):
-        questions = [
-        inquirer.List('difficulty',
-                        message=received_message['content'],
-                        choices=['Normal', 'Difícil'],
-                    ),
-        ]
+        questions = [inquirer.List(
+                                   'difficulty', 
+                                    message=received_message['content'], 
+                                    choices=['Normal', 'Difícil'], 
+                                    carousel=True
+                                    )]
         
         selected_difficulty = inquirer.prompt(questions)['difficulty']
         
-        print('Você escolheu a dificuldade ' + colored(selected_difficulty, "green" if selected_difficulty == 'Normal' else 'red') + '!\n')
+        colored_difficulty = colored(
+                                    selected_difficulty, 
+                                    'green' if selected_difficulty == 'Normal' 
+                                    else 'red'
+                                    )
         
-        message = pickle.dumps({
-            'type': 'difficulty_selection',
-            'content': selected_difficulty
-        })
+        print('Você escolheu a dificuldade ' + colored_difficulty + '!\n')
         
-        socket.send(message)
+        message['type'] = 'difficulty_selection'
+        message['content'] = selected_difficulty
         
+    # Tentativa de adivinhação
     elif(msg_type == 'guess'):
-        console.log(received_message)
         show_table(received_message['board'], received_message['secret_word'])
-        
-        if received_message['content'] == 'Você acertou!':
-            break
         
         guess = input().strip().replace(' ', '').upper()
         
-        # Send the difficulty to the server
-        message = pickle.dumps({
-            'type': 'guess',
-            'content': guess
-        })
-        
-        socket.send(message)
+        message['type'] = 'guess'
+        message['content'] = guess
     
+    # Resultado da adivinhação
     elif(msg_type == 'guess_result'):
-        
         show_table(received_message['board'], received_message['secret_word'])
         
         if(received_message['content']['game_over']):
@@ -162,28 +153,28 @@ while True:
             if(not received_message['content']['winner']):
                 print('A palavra era: ' + received_message['content']['secret_word'])
                 
-            message = pickle.dumps({
-                'type': 'game_over',
-                'content': ''
-            })
+                
+            message['type'] = 'game_over'
+            message['content'] = ''
             
+            message = pickle.dumps(message)
             socket.send(message)
             
             break
-        else:
-            guess = input('Digite a palavra: ').strip().replace(' ', '').upper()
         
-            message = pickle.dumps({
-                'type': 'guess',
-                'content': guess
-            })
-            
-            socket.send(message)
-                    
-    elif(
-        msg_type == 'update'
-    ):
+        else:
+            guess = input('Digite a palavra: ')
+            guess = guess.strip().replace(' ', '').upper()
+        
+            message['type'] = 'guess'
+            message['content'] = guess
+    
+    # Atualização do servidor
+    elif(msg_type == 'update'):
         print(received_message['content'])
-    else:
-        print('Erro desconhecido')  
+    
+    # Envia a mensagem ao servidor    
+    message = pickle.dumps(message)
+    socket.send(message)
+
 
